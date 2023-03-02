f428bea56030967e68b659c51d50e75641da5f45
Adith082 <adibhasan35@gmail.com>
2023-03-02 13:00:06 +0800

    uiupgrade on rating

--- lib/modelChoiceUser.dart
+++ lib/modelChoiceUser.dart
@@ -7,16 +7,47 @@ import 'workFlowUser.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ModelChoiceUser extends StatefulWidget {
   /// const ModelChoiceUser({Key key}) : super(key: key);
   User user;
+
   ModelChoiceUser({this.user});
   @override
   State<ModelChoiceUser> createState() => _ModelChoiceUserState();
 }
 
 class _ModelChoiceUserState extends State<ModelChoiceUser> {
+  bool likeModel1 = false;
+  var _count1 = 0;
+  var _count2 = 0;
+  bool likeModel2 = false;
+
+  void initState() {
+    // showAlert(context);
+    // super.initState();
+    // print(widget.user.uid);
+    // AlertDialog(title: Text("Sample Alert Dialog"));
+    FirebaseFirestore.instance
+        .collection("model1likecount")
+        .doc("lLSCaLxWrIZnUB2B1LYL")
+        .get()
+        .then((DocumentSnapshot doc) {
+      final data = doc.data() as Map<String, dynamic>;
+      _count1 = data["model1"];
+    });
+
+    FirebaseFirestore.instance
+        .collection("model2likecount")
+        .doc("NgnrwiZLdK6oHHzq4h1F")
+        .get()
+        .then((DocumentSnapshot doc) {
+      final data = doc.data() as Map<String, dynamic>;
+      _count2 = data["model2"];
+    });
+  }
+
   Future signOut() async {
     await GoogleSignIn().signOut();
     await FirebaseAuth.instance.signOut();
@@ -67,7 +98,7 @@ class _ModelChoiceUserState extends State<ModelChoiceUser> {
           ),
           SizedBox(height: 70),
           Container(
-            //   margin: const EdgeInsets.fromLTRB(40.0, 0, 0, 0),
+            margin: const EdgeInsets.fromLTRB(40.0, 0, 20, 0),
             decoration: const BoxDecoration(
               boxShadow: [
                 BoxShadow(
@@ -77,7 +108,7 @@ class _ModelChoiceUserState extends State<ModelChoiceUser> {
                 )
               ],
             ),
-            margin: const EdgeInsets.fromLTRB(80.0, 0, 30, 0),
+            //     margin: const EdgeInsets.fromLTRB(80.0, 0, 30, 0),
             // padding: const EdgeInsets.all(10.0),
             child: ElevatedButton(
               onPressed: () {
@@ -101,7 +132,32 @@ class _ModelChoiceUserState extends State<ModelChoiceUser> {
               ),
             ),
           ),
-          SizedBox(height: 70),
+
+          // main: MainAxisAlignment.center,
+          Container(
+            // padding: const EdgeInsets.all(1.0),
+            margin: const EdgeInsets.fromLTRB(0, 0, 100, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    likeModel1 ? Icons.favorite : Icons.favorite_border,
+                    color: likeModel1 ? Colors.red : null,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      likeModel1 = !likeModel1;
+                      _count1 += likeModel1 ? 1 : -1;
+                    });
+                  },
+                ),
+                Text('$_count1'),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 30),
           Container(
             decoration: const BoxDecoration(
               boxShadow: [
@@ -112,7 +168,7 @@ class _ModelChoiceUserState extends State<ModelChoiceUser> {
                 )
               ],
             ),
-            margin: const EdgeInsets.fromLTRB(80, 0, 30, 0),
+            margin: const EdgeInsets.fromLTRB(80, 0, 50, 0),
             // padding: const EdgeInsets.all(10.0),
             child: ElevatedButton(
               onPressed: () {
@@ -134,6 +190,28 @@ class _ModelChoiceUserState extends State<ModelChoiceUser> {
                   style: TextStyle(color: Colors.red[50], fontSize: 35),
                 ),
               ),
+            ),
+          ),
+          Container(
+            // padding: const EdgeInsets.all(1.0),
+            margin: const EdgeInsets.fromLTRB(0, 0, 100, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    likeModel2 ? Icons.favorite : Icons.favorite_border,
+                    color: likeModel2 ? Colors.red : null,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      likeModel2 = !likeModel2;
+                      _count2 += likeModel2 ? 1 : -1;
+                    });
+                  },
+                ),
+                Text('$_count2'),
+              ],
             ),
           ),
         ],
